Fix audio scaling and signal clipping issue

src/file_io.py
@@ -33,7 +33,7 @@ def write_signal_to_wav(signal: np.ndarray, wav_file_path: str, sample_rate: int
     int16_min = np.iinfo(np.int16).min
     if np.any(signal > int16_max) or np.any(signal < int16_min):
         print("Warning: Signal is clipping. It will be scaled to fit int16 range.")
-        signal = scale_signal(signal)
+    signal = scale_signal(signal)
 
     if len(signal.shape) == 1:
         channels = 1

src/snr.py
@@ -16,7 +16,7 @@ def calculate_power(signal: np.ndarray) -> float:
 
 
 def calculate_snr(signal_s: np.ndarray, signal_n: np.ndarray,
-                  n_data_s: int, n_data_n: int) -> float:
+                  n_data_s: int = 1, n_data_n: int = 1) -> float:
     power_s = calculate_power(signal_s) / n_data_s
     power_n = calculate_power(signal_n) / n_data_n
     snr = 10 * np.log10(power_s / power_n)